3a513e18d9df0ea28fb89addbb9a60a7089b4c0b
vnStrawHat <vnStrawHat@users.noreply.github.com>
2019-06-11 20:46:51 +0700

    Init site

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+echo "Deploying updates to GitHub..."
+hugo
+git add -A
+git commit -m "Updating site"
+git push origin hugo-src
+git subtree push --prefix=public https://github.com/vnStrawHat/vnstrawhat.github.io.git master
